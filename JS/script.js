2c2dfda5d41329d914831871620b74a071b57348
LucasGrataoPe12 <108188844+LucasGrataoPe12@users.noreply.github.com>
2023-05-24 17:02:37 -0300

    jogo da velha

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,147 @@
-alert("Ola Mundo")
+let bnt1 = 0;
+let bnt2 = 0;
+let bnt3 = 0;
+let bnt4 = 0;
+let bnt5 = 0;
+let bnt6 = 0;
+let bnt7 = 0;
+let bnt8 = 0;
+let bnt9 =0;
+let controle =0;
+let finalJogo = Boolean(false)
+
+function clickBtn1() {
+    let btn = document.getElementById('btn1');
+    if(btn.value == " " && !finalJogo){
+        if(controle == 0);
+        controle = 1
+        btn.value = "X"
+        btn1 = "X"
+    
+    }else{
+        controle = 0
+        btn.value = "0"
+        btn1 = "0"
+
+         }
+    }
+    function clickBtn2() {
+        let btn = document.getElementById('btn2');
+        if(btn.value == " " && !finalJogo){
+            if(controle == 0);
+            controle = 1
+            btn.value = "X"
+            btn1 = "X"
+        
+        }else{
+            controle = 0
+            btn.value = "0"
+            btn1 = "0"
+    
+             }
+        }
+        
+        function clickBtn3() {
+            let btn = document.getElementById('btn3');
+            if(btn.value == " " && !finalJogo){
+                if(controle == 0);
+                controle = 1
+                btn.value = "X"
+                btn1 = "X"
+            
+            }else{
+                controle = 0
+                btn.value = "0"
+                btn1 = "0"
+        
+                 }
+            }function clickBtn4() {
+                let btn = document.getElementById('btn1');
+                if(btn.value == " " && !finalJogo){
+                    if(controle == 0);
+                    controle = 1
+                    btn.value = "X"
+                    btn1 = "X"
+                
+                }else{
+                    controle = 0
+                    btn.value = "0"
+                    btn1 = "0"
+            
+                     }
+                }
+                function clickBtn5() {
+                    let btn = document.getElementById('btn1');
+                    if(btn.value == " " && !finalJogo){
+                        if(controle == 0);
+                        controle = 1
+                        btn.value = "X"
+                        btn1 = "X"
+                    
+                    }else{
+                        controle = 0
+                        btn.value = "0"
+                        btn1 = "0"
+                
+                         }
+                    }
+                    function clickBtn6() {
+                        let btn = document.getElementById('btn1');
+                        if(btn.value == " " && !finalJogo){
+                            if(controle == 0);
+                            controle = 1
+                            btn.value = "X"
+                            btn1 = "X"
+                        
+                        }else{
+                            controle = 0
+                            btn.value = "0"
+                            btn1 = "0"
+                    
+                             }
+                        }
+                        function clickBtn7() {
+                            let btn = document.getElementById('btn1');
+                            if(btn.value == " " && !finalJogo){
+                                if(controle == 0);
+                                controle = 1
+                                btn.value = "X"
+                                btn1 = "X"
+                            
+                            }else{
+                                controle = 0
+                                btn.value = "0"
+                                btn1 = "0"
+                        
+                                 }
+                            }
+                            function clickBtn8() {
+                                let btn = document.getElementById('btn1');
+                                if(btn.value == " " && !finalJogo){
+                                    if(controle == 0);
+                                    controle = 1
+                                    btn.value = "X"
+                                    btn1 = "X"
+                                
+                                }else{
+                                    controle = 0
+                                    btn.value = "0"
+                                    btn1 = "0"
+                            
+                                     }
+                                }
+                                function clickBtn9() {
+                                    let btn = document.getElementById('btn1');
+                                    if(btn.value == " " && !finalJogo){
+                                        if(controle == 0);
+                                        controle = 1
+                                        btn.value = "X"
+                                        btn1 = "X"
+                                    
+                                    }else{
+                                        controle = 0
+                                        btn.value = "0"
+                                        btn1 = "0"
+                                
+                                         }
+                                    }
